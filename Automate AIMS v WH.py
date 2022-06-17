e93366647d760d49c418ba7c9dbdcd3b732c6bf7
Josh9753 <107698574+Josh9753@users.noreply.github.com>
2022-06-17 12:34:14 -0400

    Update and rename Test.py to Automate AIMS v WH.py

---
+++ Automate AIMS v WH.py
@@ -0,0 +1,6 @@
+print('hello')
+print('Test 8')
+print('test John Change')
+
+
+# first project automate report for AIMS Stock vs WH Stock
--- Test.py
+++
@@ -1,3 +0,0 @@
-print('hello')
-print('Test 8')
-print('test John Change')
